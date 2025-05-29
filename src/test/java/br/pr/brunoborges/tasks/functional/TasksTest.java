9ed5e30ef9c7fe0ef8ad1c47ef79c40d0b2e3ea5
Bruno José Borges <bruno.borges@dbserver.com.br>
2025-05-29 15:49:16 -0300

    paralelismo em docker

--- src/test/java/br/pr/brunoborges/tasks/functional/TasksTest.java
+++ src/test/java/br/pr/brunoborges/tasks/functional/TasksTest.java
@@ -1,24 +1,29 @@
 package br.pr.brunoborges.tasks.functional;
 
+import java.net.URL;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 	
 	public WebDriver acessarAplicacao() {
-		WebDriver driver = new ChromeDriver();
-		
-		//Navegar até a página
-		driver.navigate().to("http://localhost:8001/tasks");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		return driver;
+		 try {
+	        ChromeOptions options = new ChromeOptions();
+	        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.4.95:4444/wd/hub"), options); // URL do Selenium Grid
+
+	        driver.navigate().to("http://192.168.4.95:8001/tasks");
+	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+	        return driver;
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
 	}
 	
 	
@@ -50,8 +55,6 @@ public class TasksTest {
 		WebDriver driver = acessarAplicacao();
 		
 		try {
-			//Navegar até a página
-			driver.navigate().to("http://localhost:8001/tasks");
 			
 			//Clicar no botão para adicionar uma nova task
 			driver.findElement(By.id("addTodo")).click();
@@ -77,8 +80,6 @@ public class TasksTest {
 		WebDriver driver = acessarAplicacao();
 		
 		try {
-			//Navegar até a página
-			driver.navigate().to("http://localhost:8001/tasks");
 			
 			//Clicar no botão para adicionar uma nova task
 			driver.findElement(By.id("addTodo")).click();
